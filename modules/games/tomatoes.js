1be5f8ba705141e4db9af911348f51299edf4cb5
ofkindness <andrei.tretyakov@gmail.com>
2018-12-27 07:12:05 +0300

    - Add tomatoes

---
+++ modules/games/tomatoes.js
@@ -0,0 +1,34 @@
+const bugsnag = require('@bugsnag/js');
+const { format } = require('util');
+
+const { dispatcher } = require('../bot');
+const Player = require('../models/player');
+const { declOfNum3 } = require('../utils');
+
+const { notify } = bugsnag(process.env.BUGSNAG_API_KEY);
+
+dispatcher.command('/tomatoes', async (req, res) => {
+  const { chat: { id: chatId } } = req;
+  const player = new Player(chatId);
+
+  try {
+    const holders = await player.storage();
+    const result = [];
+    let num = 0;
+    holders.shift();
+    holders.forEach((val, i, arr) => {
+      if (i % 2 !== 0 && i !== 0) {
+        if (parseInt(val, 10) || 0) {
+          num += 1;
+          result.push(
+            `${num}) ${arr[i - 1]} ${(parseInt(val, 10) || 0)} ${declOfNum3((parseInt(val, 10) || 0), ['томат', 'томата', 'томатов'])}`
+          );
+        }
+      }
+    });
+    const all = format('Держатели томатов:\n\n%s', result.join('\n'));
+    return res.sendMessage(chatId, all);
+  } catch (e) {
+    return notify(e);
+  }
+});
