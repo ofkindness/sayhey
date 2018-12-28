dda6f80538ab3e7e4685eb2d0966ef2be1aa3976
ofkindness <andrei.tretyakov@gmail.com>
2018-12-28 11:02:46 +0300

    - Remove format

--- modules/games/heyoftheday/index.js
+++ modules/games/heyoftheday/index.js
@@ -1,7 +1,7 @@
 const bugsnag = require('@bugsnag/js');
 const i18next = require('i18next');
 const i18nextBackend = require('i18next-node-fs-backend');
-const { format, promisify } = require('util');
+const { promisify } = require('util');
 
 const { dispatcher } = require('../../bot');
 const limiter = require('../../limiter');
@@ -22,8 +22,7 @@ const { notify } = bugsnag(process.env.BUGSNAG_API_KEY);
 const timeout = promisify(setTimeout);
 
 dispatcher.command('/hey', async (req, res) => {
-  const { chat: { id: chatId }, from: initiator } = req;
-  const { language_code: lng } = initiator;
+  const { chat: { id: chatId }, from, from: { language_code: lng } } = req;
   const game = new Game(chatId);
   const member = new Member(chatId);
   const player = new Player(chatId);
@@ -31,40 +30,34 @@ dispatcher.command('/hey', async (req, res) => {
   try {
     await i18next.use(i18nextBackend).init(Object.assign(i18nextOptions, { lng }));
 
-    const initiatorId = getId(initiator);
-    const initiatorName = getName(initiator);
-
-    if (await player.exists(initiatorId)) {
+    if (await player.exists(getId(from))) {
       if (await game.exists()) {
-        const winnerName = await game.winner();
-
-        return res.sendMessage(chatId, format(i18next.t('winner'), initiatorName, winnerName));
+        return res.sendMessage(chatId, i18next.t('winner', { winner: await game.winner() }));
       }
       // incr award
-      await player.incr(initiatorId);
+      await player.incr(getId(from));
 
-      const winnerName = await player.random();
+      const winner = await player.random();
 
-      await game.play(winnerName);
+      await game.play(winner);
 
-      await member.add(winnerName);
+      await member.add(winner);
 
-      await timeout(2000);
+      await timeout(1000);
 
-      return res.sendMessage(chatId, format(i18next.t('play'), winnerName));
+      return res.sendMessage(chatId, i18next.t('play', { winner }));
     }
     // add initiator to the game
-    await player.add(initiatorId, initiatorName);
+    await player.add(getId(from), getName(from));
     // welcome message
-    return res.sendMessage(chatId, format(i18next.t('welcome'), initiatorName));
+    return res.sendMessage(chatId, i18next.t('welcome', { username: getName(from) }));
   } catch (e) {
     return notify(e);
   }
 });
 
 dispatcher.command('/heynour', async (req, res) => {
-  const { chat: { id: chatId }, from: initiator } = req;
-  const { language_code: lng } = initiator;
+  const { chat: { id: chatId }, from, from: { language_code: lng } } = req;
 
   try {
     await i18next.use(i18nextBackend).init(Object.assign(i18nextOptions, { lng }));
@@ -76,10 +69,24 @@ dispatcher.command('/heynour', async (req, res) => {
       const results = await member.best();
       const count = await player.count();
 
-      return res.sendMessage(chatId, format(i18next.t('top'), results.join('\n'), count));
+      const players = [];
+      let num = 0;
+      results.forEach((val, i, arr) => {
+        if (i % 2 !== 0 && i !== 0) {
+          num += 1;
+          players.push(i18next.t('player', {
+            num,
+            username: arr[i - 1],
+            count: parseInt(val, 10) || 0
+          }));
+        }
+      });
+
+      return res.sendMessage(chatId,
+        i18next.t('top', { players: players.join('\n'), count }));
     }
 
-    return res.sendMessage(chatId, format(i18next.t('limit'), getName(initiator)));
+    return res.sendMessage(chatId, i18next.t('limit', { username: getName(from) }));
   } catch (e) {
     return notify(e);
   }
--- modules/games/tomatoes/index.js
+++ modules/games/tomatoes/index.js
@@ -1,7 +1,6 @@
 const bugsnag = require('@bugsnag/js');
 const i18next = require('i18next');
 const i18nextBackend = require('i18next-node-fs-backend');
-const { format } = require('util');
 
 const { dispatcher } = require('../../bot');
 
@@ -15,7 +14,7 @@ const i18nextOptions = {
   debug: true
 };
 const Player = require('../../models/player');
-const { declOfNum3 } = require('../../utils');
+// const { declOfNum3 } = require('../../utils');
 
 const { notify } = bugsnag(process.env.BUGSNAG_API_KEY);
 
@@ -27,22 +26,23 @@ dispatcher.command('/tomatoes', async (req, res) => {
   const player = new Player(chatId);
 
   try {
-    const holders = await player.storage();
-    const result = [];
+    const result = await player.storage();
+    const holders = [];
     let num = 0;
-    holders.shift();
-    holders.forEach((val, i, arr) => {
+    result.shift();
+    result.forEach((val, i, arr) => {
       if (i % 2 !== 0 && i !== 0) {
-        if (parseInt(val, 10) || 0) {
-          num += 1;
-          result.push(
-            `${num}) ${arr[i - 1]} ${(parseInt(val, 10) || 0)} ${declOfNum3((parseInt(val, 10) || 0), ['томат', 'томата', 'томатов'])}`
-          );
-        }
+        num += 1;
+        holders.push(i18next.t('holder', {
+          num,
+          username: arr[i - 1],
+          count: parseInt(val, 10) || 0
+        }));
       }
     });
-    const all = format(i18next.t('holders'), result.join('\n'));
-    return res.sendMessage(chatId, all);
+
+    return res.sendMessage(chatId,
+      i18next.t('holders', { holders: holders.join('\n') }));
   } catch (e) {
     return notify(e);
   }
